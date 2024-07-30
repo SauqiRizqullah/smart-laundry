78a80ba97ea60fbbada2c7891acb413c20f87c29
Fachrul Faathirullah <fachrulfaathir36@gmail.com>
2024-07-30 13:23:26 -0700

    hapus folder

--- Mobile/constants/images.js
+++
@@ -1,9 +0,0 @@
-import profile from "../assets/images/profile.png";
-import thumbnail from "../assets/images/thumbnail.png";
-import cards from "../assets/images/cards.png";
-import path from "../assets/images/path.png";
-import logo from "../assets/images/logo.png";
-import empty from "../assets/images/empty.png";
-import shoes_color from "../assets/images/shoes-color.png";
-
-export default { profile, thumbnail, cards, path, logo, empty, shoes_color };
